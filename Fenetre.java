51d012cbc882e028bf199e576f61cf84cb300a6a
alexandrebulatovic <alexandrebulatovic@users.noreply.github.com>
2016-11-08 00:22:53 +0100

    version fonctionnelle et commentée
    cette version fonctionne mais il faut le fichier de drivers ojdbc14.jar à télécharger sur le site de oracle (il faut s'inscrire au site) et il faut prendre la version pour "oracle database 10.2.0.1.0" sinon la connexion échouera.
    
    les tests sont inclus dans le code aussi;
    
    pour se connecter à d'autres bases de données il faut juste changer l'URL et le driver;
    
    c'est une ébauche à améliorer en créant d'autres classes éventuellement... ;

--- Fenetre.java
+++ Fenetre.java
@@ -8,8 +8,9 @@ public class Fenetre extends JFrame implements ActionListener {
 	JTextField input1, input2, input3;
 
 	public Fenetre() {
-		this.setLayout(null);
+		this.setLayout(null); // On met le layout a NULL pour tout placement manuellement avec x,y
 
+		/* CREATION DES LABELS ET DES TEXTFIELDS */
 		JLabel label1 = new JLabel("URL du serveur :");
 		label1.setBounds(30, 20, 200, 20);
 		this.add(label1);
@@ -35,10 +36,11 @@ public class Fenetre extends JFrame implements ActionListener {
 		input3.setBounds(30, 200, 200, 20);
 		this.add(input3);
 
+		/* CREATION DU BOUTON DE VALIDATION */
 		valider = new JButton("Se connecter");
 		valider.setBounds(30, 250, 150, 30);
 		valider.setActionCommand("OK");
-		valider.addActionListener(this);
+		valider.addActionListener(this); // on met un listener sur le bouton pour permettre l'interaction
 		this.add(valider);
 
 		this.setTitle(" Connexion "); //Définit un titre pour notre fenêtre
@@ -50,35 +52,48 @@ public class Fenetre extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+
 		String url, user, passwd;
 
-		if(e.getSource() == valider ){
+		if(e.getSource() == valider ){ // en cas d'appui sur le bouton de connexion
+
+			/* on recupere les variables */
 			url = input1.getText();
 			user = input2.getText();
 			passwd = input3.getText();
 
 
-			try {
-				Class.forName("oracle.jdbc.OracleDriver");
-				System.out.println("Driver OK");
+			try { // on essaie de se connecter à la base oracle
+
+				/* 
+					necessite le fichier de driver ojdbc14.jar telechargeable sur oracle.com et
+					à mettre dans C:\Program Files\Java\jre_X.XX\lib\ext\
+				 */
+
+				Class.forName("oracle.jdbc.OracleDriver"); 
+				System.out.println("Driver OK"); // si le driver est OK
 
 				Connection conn = DriverManager.getConnection(url, user, passwd);
-				System.out.println("Connexion effective !");
+				System.out.println("Connexion effective !"); // si la connexion est OK
 
 				Statement stat = conn.createStatement();
+
+				/* QUELQUES REQUETES POUR TESTER QUE TOUT FONCTIONNE */
 				stat.executeUpdate("CREATE TABLE people (id int, prenom varchar(255))");
 				stat.executeUpdate("INSERT INTO people(id,prenom) VALUES (1,'a')");
 				stat.executeUpdate("INSERT INTO people(id,prenom) VALUES (2,'b')");
 				stat.executeUpdate("INSERT INTO people(id,prenom) VALUES (3,'c')");
 
-				/* ON VERIFIE QUE LES DONNEES AIENT ETE INSEREES */
+				/* ON VERIFIE QUE LES DONNEES AIENT ETE INSEREES EN LES AFFICHANT DANS LA CONSOLE */
 				ResultSet rs = stat.executeQuery("SELECT * FROM people");  
-				
+
 				while(rs.next()) {
 					System.out.println(rs.getInt(1)+"  "+rs.getString(2));  	
 				}
 
-				stat.executeUpdate("DROP TABLE people");
+				stat.executeUpdate("DROP TABLE people"); // on nettoie la bd
+
+				/* ON FERME LA CONNEXION ET ON LIBERE LES RESSOURCES UTILISEES*/
 				conn.close();
 			}
 			catch (Exception ex) {
@@ -90,8 +105,5 @@ public class Fenetre extends JFrame implements ActionListener {
 
 	public static void main(String[] args) {
 		Fenetre fen = new Fenetre();
-
-
 	}
 }
-
