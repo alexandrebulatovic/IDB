4d0b5906fc234648956266eed6df23e9797b207b
romain_ugolini <ugolini.romain@gmail.com>
2016-11-24 18:19:48 +0100

    [Essais pour nouvelle branche]

---
+++ launch/Launcher.java
@@ -0,0 +1,25 @@
+package launch;
+
+import connect.*;
+
+import main.*;
+import java.io.File;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+import sql.SQLView;
+
+
+public class Launcher {
+
+	/**
+	 * Lance l'application
+	 * @param args : les arguments de la ligne de commande.
+	 */
+	public static void main(String[] args) 
+	{
+		ConnectionController home = new ConnectionController ();
+		System.out.println("FIN du programme.");
+	}
+
+}
