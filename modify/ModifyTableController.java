089bbba8c3f6f31e606eaf87736624e6bf7e59fa
golngaz <golngaz@laposte.net>
2016-12-05 16:10:55 +0100

    Il reste du boulot
    Ouai faut toujours retrouver la tructure des attributs... J'ai des idées
    mais ça à l'air dur je vais laisser ça comme ça en attendant

--- modify/ModifyTableController.java
+++ modify/ModifyTableController.java
@@ -1,5 +1,12 @@
 package modify;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import connect.ConnectionManager;
 import create.Attribute;
 import create.CreateTableController;
@@ -15,16 +22,54 @@ public class ModifyTableController{
 	}
 
 	public void modifier(String string) {
-		System.out.println("Hello gyes");
 		CreateTableController test = new CreateTableController(cm);
-		//Table table = new Table(,"testingge");
+		
 		test.setView(new Attribute[]{new Attribute("att1","VARCHAR",20)},"TEGTEFGTEGTEF");
 	}
 	
-	private Attribute[] getAttributes(){
+	private Attribute[] getAttributes(String tableName){
+//		this.name=name; v
+//		this.type=type; v
+//		this.size=size; v
+//		this.notNull=notNull; v
+//		this.unique=unique; 
+//		this.primaryKey=pk; v
+//		this.foreignKey=fk; 
+//		this.fkTable=fkTable;
+//		this.fkAttribute=fkAttribute;
+		//SELECT * FROm USER_TAB_COLUMNS WHERE table_name='PROFS'
 		Attribute[] attributs = null;
+		if (!this.cm.isConnected()){
+			cm.connect();
+			
+		}
+		Connection co = cm.dbms();
+		Statement st = null;
+		ResultSet rs = null;
+		String[] l = new String[3];
+		List<String[]> att = new ArrayList<String[]>();
+		
+		try {
+			st = co.createStatement();
+
+			rs = st.executeQuery("DESCRIBE "+tableName);
+			while (rs.next()){
+				l[0] = rs.getString(1);
+				l[1] = rs.getString(2);
+				l[2] = rs.getString(3);
+				att.add(l);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
+		attributs = new Attribute[att.size()];
+		for (int i = 0 ; i<att.size() ; i++){
+			attributs[i] = new Attribute(att.get(i)[0]);
+			
+		}
 		return attributs;
 		
 	}
