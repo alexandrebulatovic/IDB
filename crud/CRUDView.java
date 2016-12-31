c73419691d6b2d982a6c71d12bfa9759c1e70311
alexandrebulatovic <alexandre.bulatovic@gmail.com>
2016-12-31 03:42:47 +0100

    [ajout] creation bouton ajout/delete/update

--- crud/CRUDView.java
+++ crud/CRUDView.java
@@ -2,12 +2,16 @@ package crud;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.Vector;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
 
@@ -25,11 +29,26 @@ public class CRUDView extends BasicGUI implements ActionListener {
 	/** Liste déroulante contenant les tables disponibles. */
 	private JComboBox<String> tableComboBox;
 
-	/** {@code JScrollpane} contenant la table sélectionnée. */
+	/** Bouton pour déclencher l'action de suppression des données.*/
+	private JButton deleteButton;
+
+	/** Bouton pour déclencher l'action de mettre à jour des données.*/
+	private JButton updateButton;
+
+	/** Bouton pour déclencher l'action d'insérer des données.*/
+	private JButton insertButton;
+
+	/** {@code JScrollpane} contenant la la {@code JTable} et permettant de scroller si la taille dépasse. */
 	private JScrollPane tableScrollPane;
+
+	/** {@code JTable} correspondant à la table sélectionnée dans la combobox. */
+	private JTable tableJTable;
 
 	/** Sert à activer/désactiver l'event listener en fonction des besoins. */
 	private boolean actionListener;
+
+	/** Index de la ligne sélectionnée dans la {@code JTable}. */
+	private int index;
 
 
 	/* CONSTRUCTEUR */
@@ -39,11 +58,13 @@ public class CRUDView extends BasicGUI implements ActionListener {
 	{
 		super("CRUD", null, 900, 550, 30);
 		this.crud_controller = control;
-		this.disableListener(); // on désactive car sinon fillComboBox() déclenche l'event listener
+		this.disableListener(); // on désactive car sinon fillComboBox() appelle requestTable(...) avec l'event listener
 		this.handleComponents();
 		this.fillComboBox();
+
 		this.setProperties(WindowConstants.DISPOSE_ON_CLOSE);
 	}
+
 
 	/* METHODES */
 
@@ -57,15 +78,53 @@ public class CRUDView extends BasicGUI implements ActionListener {
 		this.actionListener = false;
 	}
 
+	/** Créé et attribue un {@code MouseListener} à notre {@code JTable}. */
+	private void initMouseListener() 
+	{
+		// obligé d'utiliser la souris et de stocker l'index en attribut car un clic sur un des JButton 
+		// déselectionne à chaque fois la ligne de la JTable et du coup getSelectedRow() retourne -1
+		// au lieu de retourner l'index de la ligne sélectionnée. */
+
+		this.tableJTable.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent evt) {
+				index = tableJTable.getSelectedRow();
+			}
+		});
+	}
+
+
 	@Override
 	public void actionPerformed(ActionEvent event) {
 
 		if (this.actionListener)
 		{
-			String tableName = (String) this.tableComboBox.getSelectedItem();
+			String selectedTable = (String) this.tableComboBox.getSelectedItem();
 
-			if (tableName != null) 
-				this.requestTable(tableName);
+			if (selectedTable != null) 
+				this.requestTable(selectedTable);
+
+
+			Object pressedButton = event.getSource();
+
+			/*
+				SI BESOIN POUR CONSTRUIRE UN OBJET TABLE : 
+				this.tableJTable.getColumnCount() --> nombre de colonnes
+				this.tableJTable.getRowCount()  --> nombre de tuples
+			 */
+			if (pressedButton == this.deleteButton) {
+
+				System.out.println("delete");
+
+			} else if (pressedButton == this.updateButton) {
+
+				System.out.println("update");
+
+			} else if (pressedButton == this.insertButton) {
+
+				System.out.println("insert");
+
+			}
+
 		}
 	}
 
@@ -75,44 +134,78 @@ public class CRUDView extends BasicGUI implements ActionListener {
 	private void requestTable(String tableName) 
 	{
 		// TODO demander la Jtable au controller puis le controller 
-		//		appelle tableDisplay(JTable correspondante...) pour mettre a jour la vue
-		//		--> les lignes dessous servent juste à tester sans controller
+		//		appelle tableDisplay(...) pour mettre a jour la vue
+		//		--> les lignes dessous servent juste à tester sans controller, on supprimera a la fin
+
+		/*
+		if (tableName.equals("test1")) {
+			Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+			Vector<String> columnNames = new Vector<String>(Arrays.asList("colonne1", "colonne2", "colonne3"));
 
 
-		/* décommentez pour tester -->
-		 * 
-		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-		Vector<String> columnNames = new Vector<String>(Arrays.asList("colonne1", "colonne2", "colonne3"));
+			for (int i = 0; i < 4; i++)
+			{
+				Vector<Object> vector = new Vector<Object>(Arrays.asList("test1"));
+				data.add(vector);
+			}
 
 
-		for (int i = 1; i< 5; i++)
-		{
-			Vector<Object> vector = new Vector<Object>(Arrays.asList("field"+i, "field"+i, "field"+i));
-			data.add(vector);
+			DefaultTableModel dm = new DefaultTableModel(data, columnNames) {
+				@Override
+				public boolean isCellEditable(int row, int column) {return false;} // empêche d'éditer les cellules de la JTable
+			};
+
+			this.tableDisplay(new JTable(dm));
+
+		} else if (tableName.equals("test2")) {
+			Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+			Vector<String> columnNames = new Vector<String>(Arrays.asList("colonne1", "colonne2", "colonne3","colonne4"));
+
+
+			for (int i = 0; i < 10; i++)
+			{
+				Vector<Object> vector = new Vector<Object>(Arrays.asList("test2"));
+				data.add(vector);
+			}
+
+			DefaultTableModel dm = new DefaultTableModel(data, columnNames) {
+				@Override
+				public boolean isCellEditable(int row, int column) {return false;} // empêche d'éditer les cellules de la JTable
+			};
+
+			this.tableDisplay(new JTable(dm));
 		}
-
-		JTable test_table = new JTable(new DefaultTableModel(data, columnNames));
-
-		this.tableDisplay(test_table);
 		 */
+
 	}
 
 	@Override
-	public boolean isComplete() { // pas d'utilité pour l'instant
+	public boolean isComplete() { // pas encore utilisé
 		return tableComboBox.getItemCount() > 0;
 	}
 
-	/** Instancie, positionne et dimensionne les différents composants de {@code this}. */
+	/** Instancie, positionne et dimensionne les différents composants de {@code this} et instancie leur {@code ActionListener} respectif. */
 	private void handleComponents()	
 	{
 		this.tableComboBox = new JComboBox<String>();
 		this.bindAndAdd(this.tableComboBox);
-
 		this.tableComboBox.addActionListener(this);
 
 		this.tableScrollPane = new JScrollPane();
-		this.bindAndAdd(this.tableScrollPane);
+		this.bindAndAdd(this.tableScrollPane, 320);
 		this.tableScrollPane.setVisible(false); // caché au début quand il n'y a pas de sélection
+
+		this.insertButton = new JButton("Insérer");
+		this.bindAndAdd(this.insertButton);
+		this.insertButton.addActionListener(this);
+
+		this.updateButton = new JButton("Mettre à jour");
+		this.bindAndAdd(this.updateButton);
+		this.updateButton.addActionListener(this);
+
+		this.deleteButton = new JButton("Supprimer");
+		this.bindAndAdd(this.deleteButton);
+		this.deleteButton.addActionListener(this);
 	}
 
 
@@ -133,13 +226,14 @@ public class CRUDView extends BasicGUI implements ActionListener {
 		}
 		this.talk(msg);
 
-		/*
-		 * decommentez pour tester -->
+		/* 
+		// commentez au-dessus et decommentez en dessous pour tester -->
 
 		this.tableComboBox.addItem("test1");
 		this.tableComboBox.addItem("test2");
 		 */
-		this.tableComboBox.setSelectedItem(null); // aucune sélection par défaut
+
+		this.tableComboBox.setSelectedItem(null); // aucune sélection par défaut pour combobox
 		this.enableListener();
 	}
 
@@ -148,8 +242,28 @@ public class CRUDView extends BasicGUI implements ActionListener {
 	 * @param table : table au format {@code JTable} à afficher. */
 	private void tableDisplay(JTable table)
 	{
-		this.tableScrollPane.setViewportView(table); // on attribue la JTable au JScrollPane
+		this.initJTable(table);
+		this.initMouseListener();
+		this.displayJTable();
+	}
+
+
+	/** Initialise et paramètre la {@code JTable} servant à l'affichage. 
+	 * @param table : {@code JTable} correspondant au choix de la combobox. */
+	private void initJTable(JTable table) 
+	{
+		this.tableJTable = table;
+		this.tableJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // une seule ligne sélectionnée autorisée
+
+	}
+
+	/** Initialise le {@code JScrollpane} et le rend visible ce qui rend l'affichage de la table actif. */
+	private void displayJTable() 
+	{
+		this.tableScrollPane.setViewportView(this.tableJTable); // on met la JTable dans le JScrollPane
 		this.tableScrollPane.setVisible(true); // on rend visible la JScrollPane qui n'est désormais plus vide
 	}
+
+
 
 }
