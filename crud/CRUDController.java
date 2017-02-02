511044cd429fd7d7f3e22e3e0ce29aeb97f36140
alexandrebulatovic <alexandre.bulatovic@gmail.com>
2017-02-02 02:30:57 +0100

    implementation bouton modifier tuple

--- crud/CRUDController.java
+++ crud/CRUDController.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.util.Vector;
 import javax.swing.JFrame;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import manager.I_DDLManager;
 import manager.SQLManager;
@@ -68,13 +70,14 @@ public class CRUDController
 	}
 
 	/**
-	 * Demande au {@code SQLManager} la {@code JTable} correspondant au nom de la table demandée.
+	 * Demande au {@code SQLManager} un modèle de {@code JTable} avec les données correspondant 
+	 * au nom de la table demandée.
 	 * @param tableName : nom de la table demandée.
-	 * @return la {@code JTable} correspondante.
+	 * @return un objet {@code DefaultTableModel} contenant les données de la table.
 	 */
-	public JTable requestTable(String tableName) 
+	public DefaultTableModel requestTable(String tableName) 
 	{
-		return this.sql_manager.requestJTable(tableName);
+		return this.sql_manager.requestTableModel(tableName);
 	}
 
 	/**
@@ -100,7 +103,7 @@ public class CRUDController
 		return (this.sql_manager.addTuple(row_to_add));
 	}
 
-	/** Met à jour une valeur d'un tuple.
+	/** Met à jour une valeur d'un tuple dans la base de données.
 	 * @param index : position du tuple.
 	 * @param column : colonne de la valeur.
 	 * @param updateBuffer : nouvelle valeur.
